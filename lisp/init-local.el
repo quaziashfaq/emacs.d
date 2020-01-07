5c511782735dadbc52f9d53448f10a2f77f9c218
Ashfaq <quazi.ashfaq@gmail.com>
2020-01-07 21:47:52 +0800

    init-local.el is updated with paths

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -4,5 +4,17 @@
 
 (global-visual-line-mode 1)
 
+(setq org-agenda-files '("~/Documents/privatenotes/gtd/inbox.org"
+                         "~/Documents/privatenotes/gtd/gtd.org"
+                         "~/Documents/privatenotes/gtd/tickler.org"))
+
+(setq org-capture-templates '(("t" "Todo [inbox]" entry
+                               (file+headline "~/Documents/privatenotes/gtd/inbox.org" "Tasks")
+                               "* TODO %i%?")
+                              ("T" "Tickler" entry
+                               (file+headline "~/Documents/privatenotes/gtd/tickler.org" "Tickler")
+                               "* %i%? \n %U")))
+
+
 (provide 'init-local)
 ;;; init-local.el ends here
