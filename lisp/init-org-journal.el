749f0c52cf93be130f8cac31b17b16173722c5db
quaziashfaq <quazi.ashfaq@gmail.com>
2023-12-07 16:30:36 +0800

    Delete lisp/init-org-journal.el

--- lisp/init-org-journal.el
+++
@@ -1,9 +0,0 @@
-(require-package 'org-journal)
-
-
-(customize-set-variable 'org-journal-dir "~/Documents/privatenotes/journal")
-(customize-set-variable 'org-journal-date-format "%A, %d %B %Y")
-(customize-set-variable 'org-journal-carryover-items "TODO=\"TODO\"|TODO=\"NEXT\"|TODO=\"PROJECT\"|TODO=\"WAITING\"|TODO=\"DELEGATED\"|TODO=\"HOLD\"")
-(require 'org-journal)
-
-(provide 'init-org-journal)
